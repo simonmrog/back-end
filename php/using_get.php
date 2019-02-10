adf6fe0ba871decf4a85e26b398277bbb54f33f8
simonmrog <simonmrog@gmail.com>
2019-02-10 13:26:34 -0500

    using GET

--- php/using_get.php
+++ php/using_get.php
@@ -6,7 +6,11 @@
 <body>
 
 	<?php
-		$val = $_GET["number"];
+		if (isset($_GET["number"]) and is_numeric($_GET["number"]))
+			$val = $_GET["number"];
+		else
+			$val = 0;
+
 		echo "<h2> Tabla de multiplicar </h2> <br/>";
 		for ($i = 1; $i <= 10; $i++) {
 			echo $val . "x" . $i . " = " . ($val * $i) . "<br/>";
